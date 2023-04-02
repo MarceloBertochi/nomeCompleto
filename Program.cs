d5f1cea0d5f2e03fe8af1236da15fbec84a519d3
MarceloBertochi <paidasamia@gmail.com>
2023-04-02 18:22:56 -0300

    Exercicio de Nome Completo concluido

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("**/-- Exercício de Nome Completo --/**");
+Console.WriteLine();
+string nome, sobrenome;
+
+Console.WriteLine("Digite seu nome");
+nome = Console.ReadLine()!;
+Console.WriteLine();
+
+Console.WriteLine("Digite seu Sobrenome");
+sobrenome = Console.ReadLine()!;
+Console.WriteLine();
+
+Console.WriteLine($"Nome Completo :.. {nome} {sobrenome}\n");
+
+Console.WriteLine($"Nome de catálogo :.. {sobrenome.ToUpper()}, {nome}\n");
+
+//Consegui fazer esse exercio sem olhar nada na correção. 
